7c0f6c5cc8c658e9eb5078415273ad30f3c27b5c
iratcoindo <iratco.indo@gmail.com>
2026-07-14 09:36:34 +0700

    Update auth.py

--- Modules/auth.py
+++ Modules/auth.py
@@ -1,1 +1,26 @@
+import pandas as pd
 
+DATABASE = "data/iRATco_Database.xlsx"
+
+
+def login(username, password):
+    """
+    Mengembalikan data user jika login berhasil.
+    Mengembalikan None jika gagal.
+    """
+
+    users = pd.read_excel(
+        DATABASE,
+        sheet_name="Users"
+    )
+
+    user = users[
+        (users["username"] == username) &
+        (users["password"] == password) &
+        (users["active"] == "Yes")
+    ]
+
+    if len(user) == 1:
+        return user.iloc[0]
+
+    return None
